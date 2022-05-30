05a2fc100af47a2a5f53dbd08bf066a8e6ec3873
Romulo Paiva <romulofpaiva@gmail.com>
2022-05-30 08:41:11 -0300

    add files

---
+++ src/main/java/com/romulofpaiva/gsaccessingmongodbdatarest/GsAccessingMongodbDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.romulofpaiva.gsaccessingmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingMongodbDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingMongodbDataRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romulofpaiva/gsaccessingmongodbdatarest/GsAccessingMongodbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulofpaiva.gsaccessingmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingMongodbDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
